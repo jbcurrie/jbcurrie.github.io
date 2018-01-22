2318466ef3269d1818b8a475398a469df99dcb80
jbcurrie <biobacker30@gmail.com>
2018-01-21 21:40:50 -0500

    mobile responsive nav

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,12 +1,7 @@
-// Get the navbar
-var navbar = document.getElementById("navbar");
-var body = document.getElementsByTagName("body");
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-var text = document.getElementsByClassName("text");
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-
-function myFunction() {
+const sticky = navbar.offsetTop;
+// sticky navbar
+function stickyNav() {
+  const navbar = document.getElementById("navbar");
   if (window.pageYOffset >= sticky) {
       
     navbar.classList.add("sticky")
@@ -15,7 +10,7 @@ function myFunction() {
     navbar.classList.remove("responsive");
   }
 }
-
+// hide scroll top button above nav bar
 function scrollFunction() {
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         document.getElementById("btn-scroll").style.display = "block";
@@ -24,21 +19,25 @@ function scrollFunction() {
     }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+// trigger scroll top
 function topFunction() {
     document.body.scrollTop = 0; // For Chrome, Safari and Opera 
     document.documentElement.scrollTop = 0; // For IE and Firefox
 }
-
+// trigger hamburger
 function hamburger() {
-    var x = document.getElementById("navbar");
-    if (x.className === "topNav sticky") {
-        x.className += " responsive";
+    const navbar = document.getElementById("navbar");
+    const addTimes = document.getElementsByClassName('hamburger')[0]
+    
+    if (navbar.className === "topNav sticky") {
+        navbar.className += " responsive";
+        addTimes.innerHTML = '&times;'
     } else {
-        x.className = "topNav";
+        navbar.className = "topNav";
+      addTimes.innerHTML = '&#9776;'
     }
 }
-
+// animate portfolios
 function runAnimate (e) {
     var target = e.currentTarget
     var content = target.getElementsByClassName("portfolio-content")[0];
@@ -51,13 +50,15 @@ function runAnimate (e) {
         header.className = "name"
     }
 }
+// animate portfolios
 function galleryAnimate (e) {
+  const text = document.getElementsByClassName("text");
     for (i = 0; i < text.length; i++) {
         text[i].addEventListener("click",runAnimate,false);
     }
 }
 
-window.onscroll = function() {scrollFunction()};
-document.addEventListener("scroll", myFunction);
-navbar.addEventListener("click",hamburger);
+window.onscroll = scrollFunction // function() {scrollFunction()};
+document.addEventListener("scroll", stickyNav);
+document.getElementById("navbar").addEventListener("click",hamburger);
 galleryAnimate();
